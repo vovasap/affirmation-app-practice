3b39c37e1dd6ed95233c8bc6c78d1dc2a9e0a19f
Vladimir <rednotgreen2020@gmail.com>
2020-07-09 00:24:22 +0300

    changed interface

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-const initApp = function() {
-    const numberOfAffirmations = document.querySelector('.number')
-    const durationOfAffirmations = document.querySelector('.duration')
-    const btnStartAndPause = document.querySelector('.btn_start-pause')
-    const btnStop = document.querySelector('.btn_stop')
-    const display = document.querySelector('.display')
+const initApp = function () {
+  const numberOfAffirmations = document.querySelector('.number');
+  const durationOfAffirmations = document.querySelector('.duration');
+  const btnStartAndPause = document.querySelector('.btn_start-pause');
+  const btnStop = document.querySelector('.btn_stop');
+  const display = document.querySelector('.display');
 
-    let time = durationOfAffirmations.value
-    let timer
-    let isPaused = true
-    let isStoped = true
+  let time = durationOfAffirmations.value;
+  let timer;
+  let isPaused = true;
+  let isStoped = true;
 
-    let startPause = function() {
-        if (isPaused) {
-            if (isStoped) {
-                time = durationOfAffirmations.value
-                display.textContent = time
-            }
-            timer = setInterval(() => {
-                display.textContent = --time
-                if (time == 0) clearInterval(timer)
-            }, 1000)
-            
-            isPaused = false
-            isStoped = false
-        } else {
-            clearInterval(timer)
-            isPaused = true
-        }
+  let startPause = function () {
+    if (isPaused) {
+      if (isStoped) {
+        time = durationOfAffirmations.value;
+        display.textContent = time;
+      }
+      timer = setInterval(() => {
+        display.textContent = --time;
+        if (time == 0) clearInterval(timer);
+      }, 1000);
+
+      isPaused = false;
+      isStoped = false;
+    } else {
+      clearInterval(timer);
+      isPaused = true;
     }
-    
-    let stop = function() {
-        clearInterval(timer)
-        isPaused = true
-        isStoped = true
-    }
+  };
 
-    btnStartAndPause.addEventListener('click', startPause)
-    btnStop.addEventListener('click', stop)
-}
+  let stop = function () {
+    clearInterval(timer);
+    isPaused = true;
+    isStoped = true;
+  };
 
-initApp()
+  btnStartAndPause.addEventListener('click', startPause);
+  btnStop.addEventListener('click', stop);
+};
+
+initApp();
